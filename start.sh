417e5ca38b08a35bb1a9d10ce16e60099822cb7d
David B <37987466+dalijolijo@users.noreply.github.com>
2018-06-10 00:17:59 +0200

    Change .gravium to .graviumcore

--- start.sh
+++ start.sh
@@ -19,24 +19,23 @@ chown gravium:gravium /tmp/gravium.conf
 # Set rpcuser, rpcpassword and masternode genkey
 #
 printf "** Set rpcuser, rpcpassword and masternode genkey ***\n"
-mkdir -p /home/gravium/.gravium/
+mkdir -p /home/gravium/.graviumcore/
 chown -R gravium:gravium /home/gravium/
-sudo -u gravium cp /tmp/gravium.conf /home/gravium/.gravium/
-sed -i "s/^\(rpcuser=\).*/rpcuser=grvmasternode${GRVPWD}/" /home/gravium/.gravium/gravium.conf
-sed -i "s/^\(rpcpassword=\).*/rpcpassword=${GRVPWD}/" /home/gravium/.gravium/gravium.conf
-sed -i "s/^\(externalip=\).*/externalip=${GRVIP}/" /home/gravium/.gravium/gravium.conf 
-sed -i "s/^\(masternodeprivkey=\).*/masternodeprivkey=${MN_KEY}/" /home/gravium/.gravium/gravium.conf
-cp /home/gravium/.gravium/gravium.conf /home/gravium/.graviumcore/gravium.conf
+sudo -u gravium cp /tmp/gravium.conf /home/gravium/.graviumcore/
+sed -i "s/^\(rpcuser=\).*/rpcuser=grvmasternode${GRVPWD}/" /home/gravium/.graviumcore/gravium.conf
+sed -i "s/^\(rpcpassword=\).*/rpcpassword=${GRVPWD}/" /home/gravium/.graviumcore/gravium.conf
+sed -i "s/^\(externalip=\).*/externalip=${GRVIP}/" /home/gravium/.graviumcore/gravium.conf 
+sed -i "s/^\(masternodeprivkey=\).*/masternodeprivkey=${MN_KEY}/" /home/gravium/.graviumcore/gravium.conf
 
 #
 # Downloading bootstrap file (not yet available)
 # Generate it with: 
-# 1) Go to blocks folder (e.g. /home/gravium/.gravium/blocks
+# 1) Go to blocks folder (e.g. /home/gravium/.graviumcore/blocks
 # 2) Execute after sync: #cat blk000*.dat > bootstrap.dat
 #
 #printf "** Downloading bootstrap file ***\n"
-#cd /home/gravium/.gravium/
-#if [ ! -d /home/gravium/.gravium/blocks ] && [ "$(curl -Is https://gravium.io/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
+#cd /home/gravium/.graviumcore/
+#if [ ! -d /home/gravium/.graviumcore/blocks ] && [ "$(curl -Is https://gravium.io/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
 #        sudo -u gravium wget https://gravium.io/${BOOTSTRAP}; \
 #        sudo -u gravium tar -xvzf ${BOOTSTRAP}; \
 #        sudo -u gravium rm ${BOOTSTRAP}; \
